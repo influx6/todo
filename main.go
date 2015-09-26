e1601ac814b9fe2356981022311966cb2b384181
Ewetumo Alexander <trinoxf@gmail.com>
2015-09-26 13:39:56 +0100

    using openshift deploy

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/influx6/todo/app"
@@ -14,6 +16,8 @@ func main() {
 			panic(err.Error())
 		}
 	}
+
+	app.App.Addr = fmt.Sprintf("%s:%s", os.Getenv("OPENSHIFT_GO_IP"), os.Getenv("OPENSHIFT_GO_PORT"))
 
 	log.Printf("%s is now up", app.App.Name)
 	app.App.Serve()
